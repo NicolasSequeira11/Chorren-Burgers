b644d2fef414398679f205d8cd7f96be924b5d4b
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-01-04 23:05:15 -0300

    Agregue SASS y ajuste responsive

--- menu.js
+++ menu.js
@@ -1,7 +1,9 @@
 
 const ham = document.querySelector('.ham');
-const enlaces = document.querySelector('.enlaces-menu');
+const enlaces = document.querySelector('.nav__links');
 
 ham.addEventListener('click', () => {
+    
     enlaces.classList.toggle('activado');
+
 });
